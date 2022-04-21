dbc7429630f8734fa87e0a443884ca28ad57b353
nickallegator <86098660+nickallegator@users.noreply.github.com>
2022-04-21 08:08:03 -0500

    Unity Project

---
+++ Class Demo/Assets/Scripts/Hazard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+	public void OnCollisionEnter(Collision other)
+	{
+		Debug.Log(other);
+		SceneManager.LoadScene("MainMenu");
+		switch (other.collider.tag)
+		{
+			case "Player":
+				Cursor.visible = true;
+				SceneManager.LoadScene("MainMenu");
+				break;
+		}
+	}
+}
---
+++ Class Demo/Assets/Scripts/ScoreBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBehavior : MonoBehaviour
+{
+   private Text thisText;
+    private static int score;
+    
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        
+        // set score value to be zero
+        score = 0;
+    }
+    
+    void Update() 
+    {
+        // When P is hit
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            // add 500 points to score
+            score += 1;
+        }
+        // update text of Text element
+        thisText.text = "Score: " + score;
+    }
+
+    public static void AddScore()
+    {
+        // add 500 points to score
+        score += 1;
+    }
+}
+
+
+
