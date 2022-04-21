fc7c0cbf66768a22f4640bef0c7b091e3145744b
nickallegator <86098660+nickallegator@users.noreply.github.com>
2022-04-21 08:32:08 -0500

    Added Health

---
+++ Class Demo/Assets/Scripts/HealthBarBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarBehaviour : MonoBehaviour
+{
+
+    public Slider hSlider;
+    Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.FindWithTag("Player").GetComponent<Health>();
+        hSlider.maxValue = playerHealth.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hSlider.value = playerHealth.health;
+    }
+}
